7976b48123982596bf8966d434417f6a95852f3b
priyanka <priyankaboopu@gmail.com>
2022-12-05 16:50:10 +0530

    priya3

---
+++ src/test/java/DemoJenkins/BuildCreationTest.java
@@ -0,0 +1,16 @@
+package DemoJenkins;
+
+import org.testng.annotations.Test;
+
+public class BuildCreationTest {
+
+	@Test
+	public void create() {
+		System.out.println("Build is created");
+	}
+	
+	@Test
+	public void update() {
+		System.out.println("Build is updated");
+	}
+}
