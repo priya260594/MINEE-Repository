e4422048d3f54030f3974361c090ad3c62924b01
priyanka <priyankaboopu@gmail.com>
2022-12-05 16:27:31 +0530

    priya1

---
+++ src/test/java/DemoJenkins/DemoTest.java
@@ -0,0 +1,21 @@
+package DemoJenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void demo1Test() {
+		System.out.println("Demo1");
+	}
+	
+	@Test
+	public void demo2Test() {
+		System.out.println("Demo2");
+	}
+	
+	@Test
+	public void demo3Test() {
+		System.out.println("Demo3");
+	}
+}
